1f3946442a7d78e0be0833c28ab876d53eba699d
Michelle <mtrusgn0802@att.net>
2018-11-26 11:53:27 -0600

    Finished 3.4 displayed list of cheese - index.cshtml form to add cheese - add.cshtml post request of 'add' form -  a new controller = NewCheese()

--- CheeseMVC/Controllers/CheeseController.cs
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -10,10 +10,35 @@ namespace CheeseMVC.Controllers
 {
     public class CheeseController : Controller
     {
+        //static - is available to any code within this class
+        //instance variable or property of a class is capitalized
+        //local variable is lower case
+        //a list of cheeses that belong to the controller so that you can use across different controller methods
+        static private List<String> Cheeses = new List<String>();
         // GET: /<controller>/
         public IActionResult Index()
         {
+            ViewBag.cheeses = Cheeses;
+
             return View();
         }
+
+        //displays the Add form
+        public IActionResult Add()
+        {
+            return View();
+        }
+        
+        [Route("Cheese/Add")]
+        [HttpPost]
+        //handles the (Add) form submission
+        public IActionResult NewCheese(string name)
+        {
+            //add new cheese to the list of cheeses
+            Cheeses.Add(name);
+            return Redirect("/Cheese");
+
+        }
+    
     }
 }
